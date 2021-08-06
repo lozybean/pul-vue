67ed3b555a52d76aa39f7dcb4231d8eed06a7454
codeunsolved <codeunsolved@gmail.com>
2021-08-06 20:13:47 +0800

    chore(src/view/Home.vue): revise text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,52 +10,33 @@
         >
           <div>
             <h1>
-              What is Sift-PULs (Specific functional Polysaccharide utilization
-              locus)?
+              1. What is Sift-PULs database(Specific functional Polysaccharide utilization locus)?
             </h1>
             <p>
-              Polysaccharide utilization locus(PUL) is bacterial gene cluster
-              including genes responsible for multiple biological processes
-              (polysaccharide degradation, sugar transporting and metabolism).
-              PUL studies are blooming in recent years but the biochemical
-              characterization is relative slow. There is a demand for PUL
-              database with function prediction. In term of this problem,
-              Sift-PULs is proposed, not only to predict PULs, but also to
-              assign specific functions to each PUL.
+              Sift-PUL database is a public repository providing predicted PULs with specific functional annotations. The key difference between sift-PUL and other PUL databases (e.g. PULDB, CGCs and dbCAN-PUL) is the functional annotation for each predicted PUL.
             </p>
 
-            <h1>How do we predict Sift-PULs?</h1>
+            <h1>2. Signature genes specific for each polysaccharide are used for Sift-PUL prediction</h1>
             <p>
-              By analyzing biochemically characterized PULs, we identified
-              signature genes that specific for alginate, agar, carrageenan,
-              chitin, beta-mannan, pectin and xylan, respectively. Using
-              signature genes, PULs with specific functions were predicted.
+              The key to predicting PUL is what signature genes (also called as marker genes) are selected, for example SusC/D and carbohydrate active enzymes are used in PULDB and transcription factor, sugar transporter and carbohydrate active enzymes are used in CGCs. Signature genes used for Sift-PUL prediction are specific for each polysaccharide and are classified into two categories: core genes (backbone monosaccharide’s metabolic genes) and alternative genes (carbohydrate active enzymes). 
             </p>
 
-            <h1>Advantage of Sift-PULs?</h1>
+            <h1>3. How do we predict sift-PULs?</h1>
             <p>
-              • <strong>Specificity</strong>: We assign a specific function to
-              each PUL.<br />
-              • <strong>Diversity</strong>: PULs come from different phyla other
-              than <i>Bacteroidetes</i>. <br />
+              A sift-PUL specific for one polysaccharide must have all corresponding core genes and at least one corresponding alternative gene. To ensure specificity, most Hmmer models of core genes and alternative genes were rebuilt locally. 
             </p>
 
             <p>
               <br />
               <strong>Development of Sift-PULs:</strong><br />
-              2014.06-2016.06: Agar PUL prediction using conserved metabolic
-              genes<br />
-              (Finished by Dr. Tao Song and Mr Congchong Wei in Sichuan
-              University)<br />
-              2016.06-2018.06: Discovery of novel enzymes and novel pathways
-              based on predicted agar PULs.<br />
-              (Finished by Dr. Tao Song)<br />
-              2018.06-2021.06: Website construction<br />
-              (Finished by Dr. Tao Song and Mr Congchong Wei in Chengdu
-              University)
+              2014.06-2016.06: Agar PUL prediction using conserved metabolic genes (Dr. Tao Song and Mr Congchong Wei in Sichuan University)<br />
+
+              2016.06-2018.06: Discovery of novel enzymes and novel pathways based on predicted agar PULs.(Dr. Tao Song)<br />
+
+              2018.06-2021.06: Website construction (Dr. Tao Song and Mr Congchong Wei in Chengdu University)<br />
             </p>
             <p>
-              If you have any questions, please contact us. <br />
+              If you have any questions, please contact us.<br />
               songtao@cdu.edu.cn
             </p>
           </div>
