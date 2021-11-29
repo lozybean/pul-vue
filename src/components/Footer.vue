1ba90495efcf77a9ecacb6f4778dcae491a58c4c
liangzebin <liangzebin5566@gmail.com>
2021-11-29 10:47:16 +0800

    fix: 网站访问统计仅在首页展示

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,6 @@
           <a href="#"> songtao@cdu.edu.cn</a></span
         >
       </div>
-      <div id="clustrmaps-container">
-        <remote-script
-          id="clustrmaps"
-          src="//clustrmaps.com/map_v2.js?d=UdMFN_HC3kswSnbZNRw8bblEOI8UjjRjiGpSK9isClE&cl=ffffff&w=a"
-        ></remote-script>
-      </div>
     </div>
   </el-footer>
 </template>
@@ -35,9 +29,5 @@ export default {
 .footer-main {
   font-size: 10px;
   color: #909399;
-}
-#clustrmaps-container {
-  width: 20%;
-  height: 100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,30 +10,50 @@
         >
           <div>
             <h1>
-              1. What is Sift-PULs database(Specific functional Polysaccharide utilization locus)?
+              1. What is Sift-PULs database(Specific functional Polysaccharide
+              utilization locus)?
             </h1>
             <p>
-              Sift-PUL database is a public repository providing predicted PULs with specific functional annotations. The key difference between sift-PUL and other PUL databases (e.g. PULDB, CGCs and dbCAN-PUL) is the functional annotation for each predicted PUL.
+              Sift-PUL database is a public repository providing predicted PULs
+              with specific functional annotations. The key difference between
+              sift-PUL and other PUL databases (e.g. PULDB, CGCs and dbCAN-PUL)
+              is the functional annotation for each predicted PUL.
             </p>
 
-            <h1>2. Signature genes specific for each polysaccharide are used for Sift-PUL prediction</h1>
+            <h1>
+              2. Signature genes specific for each polysaccharide are used for
+              Sift-PUL prediction
+            </h1>
             <p>
-              The key to predicting PUL is what signature genes (also called as marker genes) are selected, for example SusC/D and carbohydrate active enzymes are used in PULDB and transcription factor, sugar transporter and carbohydrate active enzymes are used in CGCs. Signature genes used for Sift-PUL prediction are specific for each polysaccharide and are classified into two categories: core genes (backbone monosaccharide’s metabolic genes) and alternative genes (carbohydrate active enzymes). 
+              The key to predicting PUL is what signature genes (also called as
+              marker genes) are selected, for example SusC/D and carbohydrate
+              active enzymes are used in PULDB and transcription factor, sugar
+              transporter and carbohydrate active enzymes are used in CGCs.
+              Signature genes used for Sift-PUL prediction are specific for each
+              polysaccharide and are classified into two categories: core genes
+              (backbone monosaccharide’s metabolic genes) and alternative genes
+              (carbohydrate active enzymes).
             </p>
 
             <h1>3. How do we predict sift-PULs?</h1>
             <p>
-              A sift-PUL specific for one polysaccharide must have all corresponding core genes and at least one corresponding alternative gene. To ensure specificity, most Hmmer models of core genes and alternative genes were rebuilt locally. 
+              A sift-PUL specific for one polysaccharide must have all
+              corresponding core genes and at least one corresponding
+              alternative gene. To ensure specificity, most Hmmer models of core
+              genes and alternative genes were rebuilt locally.
             </p>
 
             <p>
               <br />
               <strong>Development of Sift-PULs:</strong><br />
-              2014.06-2016.06: Agar PUL prediction using conserved metabolic genes (Dr. Tao Song and Mr Congchong Wei in Sichuan University)<br />
+              2014.06-2016.06: Agar PUL prediction using conserved metabolic
+              genes (Dr. Tao Song and Mr Congchong Wei in Sichuan University)<br />
 
-              2016.06-2018.06: Discovery of novel enzymes and novel pathways based on predicted agar PULs.(Dr. Tao Song)<br />
+              2016.06-2018.06: Discovery of novel enzymes and novel pathways
+              based on predicted agar PULs.(Dr. Tao Song)<br />
 
-              2018.06-2021.06: Website construction (Dr. Tao Song and Mr Congchong Wei in Chengdu University)<br />
+              2018.06-2021.06: Website construction (Dr. Tao Song and Mr
+              Congchong Wei in Chengdu University)<br />
             </p>
             <p>
               If you have any questions, please contact us.<br />
@@ -44,6 +64,12 @@
       </el-container>
     </el-main>
     <Footer></Footer>
+    <div id="clustrmaps-container">
+      <remote-script
+        id="clustrmaps"
+        src="//clustrmaps.com/map_v2.js?d=UdMFN_HC3kswSnbZNRw8bblEOI8UjjRjiGpSK9isClE&cl=ffffff&w=a"
+      ></remote-script>
+    </div>
   </el-container>
 </template>
 
@@ -60,3 +86,10 @@ export default {
   },
 };
 </script>
+
+<style>
+#clustrmaps-container {
+  width: 20%;
+  height: 100%;
+}
+</style>
